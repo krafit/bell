e55ac8ab2116d49da9af54e9552bec49e9fbcab6
Simon Kraft <simon@krafit.de>
2016-01-07 22:58:06 +0100

    Initial Commit

---
+++ inc/jetpack.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Jetpack Compatibility File.
+ *
+ * @link https://jetpack.me/
+ *
+ * @package krafit_bell
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.me/support/infinite-scroll/
+ * See: https://jetpack.me/support/responsive-videos/
+ */
+function krafit_bell_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'krafit_bell_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+}
+add_action( 'after_setup_theme', 'krafit_bell_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function krafit_bell_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+		    get_template_part( 'template-parts/content', 'search' );
+		else :
+		    get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+}
