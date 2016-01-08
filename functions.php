552bb3cf24153aedc734a0362b50f59327b3679d
Simon Kraft <simon@krafit.de>
2016-01-08 09:05:14 +0100

    Added .min. suffix for css and js files

--- functions.php
+++ functions.php
@@ -97,9 +97,12 @@ add_action( 'widgets_init', 'krafit_bell_widgets_init' );
  * Enqueue scripts and styles.
  */
 function krafit_bell_scripts() {
-	wp_enqueue_style( 'krafit_bell-style', get_stylesheet_uri() );
+	
+	$suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
+	
+	wp_enqueue_style( 'krafit_bell-style', get_template_directory_uri() . '/style' . $suffix . '.css' );
 
-	wp_enqueue_script( 'krafit_bell-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'krafit_bell-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix' . $suffix . '.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
